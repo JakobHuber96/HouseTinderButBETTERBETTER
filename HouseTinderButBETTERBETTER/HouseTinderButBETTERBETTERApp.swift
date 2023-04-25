c02887da438e75e9ec1e467ab949f469ed4683e2
Jakob Huber <93046@psdschools.org>
2023-04-25 09:20:39 -0600

    Initial Commit

---
+++ HouseTinderButBETTERBETTER/HouseTinderButBETTERBETTERApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseTinderButBETTERBETTERApp.swift
+//  HouseTinderButBETTERBETTER
+//
+//  Created by Huber, Jakob - Student on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct HouseTinderButBETTERBETTERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
